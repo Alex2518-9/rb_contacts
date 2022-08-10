a10c759731dbcc8edd9f2d9bf2aae2afe2e0233b
Teringer Alex <ateringer@b2international.com>
2022-08-10 17:59:13 +0200

    fix(changeCss): extend css

--- src/components/buttons/Button.jsx
+++ src/components/buttons/Button.jsx
@@ -1,11 +1,13 @@
 import React from "react";
+import { classNames } from "../../utils";
 import "./Buttons.css";
 
-const classNames = (...classNames) =>
-  classNames.filter((className) => className).join(" ");
-
 const Button = ({ children, className, onClick }) => {
-  return <button className={classNames("btn", className)} onClick={onClick}>{children}</button>;
+  return (
+    <button className={classNames("btn", className)} onClick={onClick}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
