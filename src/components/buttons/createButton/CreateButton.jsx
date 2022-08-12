8d01e7334f597930041e3827ee744594d57a8a44
Teringer Alex <ateringer@b2international.com>
2022-08-12 09:30:08 +0200

    fix(clean): cleaning code

--- src/components/buttons/createButton/CreateButton.jsx
+++ src/components/buttons/createButton/CreateButton.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import Button from '../Button';
-import './CreateButton.css';
+import React from "react";
+import Button from "../Button";
+import "./CreateButton.css";
 
-
-const CreateButton = ({onClick, children, darkTheme}) => {
+const CreateButton = ({ onClick, children, darkTheme }) => {
   return (
-    <Button className={darkTheme ? "add-dark-button" : "add-light-button"} onClick={onClick}>{children}</Button>
-  )
-}
- //className=" addBtn"
-export default CreateButton
+    <Button
+      className={darkTheme ? "add-dark-button" : "add-light-button"}
+      onClick={onClick}
+    >
+      {children}
+    </Button>
+  );
+};
+
+export default CreateButton;
