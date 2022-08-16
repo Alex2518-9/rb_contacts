4aa0190dd3585f2cf9183bb0a86716dc4bcfa2f8
Teringer Alex <ateringer@b2international.com>
2022-08-16 17:44:28 +0200

    feat(new): create searchBar and sort function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./GlobalStyle.css";
 import CreateButton from "./components/buttons/createButton/CreateButton";
@@ -9,21 +9,24 @@ import AddContactForm from "./components/forms/addContactForm/AddContactForm";
 import { BsMoon } from "react-icons/bs";
 import { BsSun } from "react-icons/bs";
 import {
-  AiOutlineEye,
   AiOutlinePlus,
-  AiOutlineEyeInvisible,
+  AiOutlineSortAscending,
+  AiOutlineSortDescending,
 } from "react-icons/ai";
+import PasswordInput from "./components/inputs/passwordInput/PasswordInput";
+//import SearchInput from "./components/inputs/searchInput/SearchInput";
 
-export const ThemeContexts = createContext();
+// export const ThemeContexts = createContext();
 
 function App() {
   const [contacts, setContacts] = useState([]);
   const [mode, setMode] = useState("home");
   const [editContactData, setEditContactData] = useState();
   const [theme, setTheme] = useState("dark");
-  const [selectedPassword, setSelectedPassword] = useState([]);
+  const [search, setSearch] = useState("");
+  const [sortByNameAZ, setSortByNameAZ] = useState(true);
+  const [sortByEmailAZ, setSortByEmailAZ] = useState(true);
 
-  console.log();
   const onDelete = (id) => {
     setContacts((previouState) =>
       previouState.filter((item) => item.id !== id)
@@ -61,121 +64,138 @@ function App() {
     setTheme("dark");
   };
 
-  const onShowAndHidePassword = (id) => {
-     const [changePasswordRow] = contacts.filter((item) => item.id === id);
-    // setSelectedPassword([...selectedPassword, changePasswordRow])
-    // selectedPassword.push(changePasswordRow)
-    // setSelectedPassword([...changePasswordRow]);
-    // const keyIndex = selectedPassword.indexOf(id);
+  const searchedContact = contacts.filter((contact) => {
+    return (
+      contact.name.toLowerCase().includes(search.toLowerCase()) ||
+      contact.email.toLowerCase().includes(search.toLowerCase())
+    );
+  });
 
-    // if (keyIndex >= 0 ) {
-    //   setSelectedPassword([...selectedPassword.slice(0, keyIndex), selectedPassword.slice(keyIndex + 1)])
-    // }else{
-    //   selectedPassword.push(id)
-    // }
-
-    // setSelectedPassword({selectedPassword});
-    
-    console.log(selectedPassword);
-    if (selectedPassword?.id === changePasswordRow.id) {
-      setSelectedPassword();
-    }else{
-      setSelectedPassword(changePasswordRow);
-    }
-  };
+  // const sortedNameList = 
+  const sortedList = [...searchedContact].sort((a, b) => a.name < b.name ? -1 : 1);
+  const sortedListCompare = [...searchedContact].sort((a, b) => a.name > b.name ? -1 : 1);
 
   return (
-    <ThemeContexts.Provider>
-      <div className={`App ${theme}`}>
-        <div className="switch">
-          <div className="switch-container">
-            <div className={theme === "dark" ? "moon" : "sun"} onClick={onDark}>
-              <BsMoon />
-            </div>
-            <div
-              className={theme === "dark" ? "sun" : "moon"}
-              onClick={onLight}
-            >
-              <BsSun />
-            </div>
+    // <ThemeContexts.Provider>
+    <div className={`App ${theme}`}>
+      <div className="switch">
+        <div className="switch-container">
+          <div className={theme === "dark" ? "moon" : "sun"} onClick={onDark}>
+            <BsMoon />
+          </div>
+          <div className={theme === "dark" ? "sun" : "moon"} onClick={onLight}>
+            <BsSun />
           </div>
         </div>
-
-        {mode === "home" && (
-          <>
-            <div className="contact-container-title">
-              <h1>Contacts</h1>
-            </div>
-            <table className={`contact-container-body`}>
-              <thead>
-                <tr>
-                  <div className="head-texts">
-                    <th>Username</th>
-                    <th>Email</th>
-                    <th>Password</th>
-                  </div>
-                  <th>Action</th>
-                </tr>
-              </thead>
-              <tbody>
-                {contacts.map(({ id, name, email, password }) => (
-                  <tr className="sor" key={id}>
-                    <td>{name}</td>
-                    <td>{email}</td>
-                    <td>
-                      <input type={selectedPassword?.id === id ? "text" : "password"} value={password} disabled />
-                      <i onClick={() => onShowAndHidePassword(id)}>
-                      {selectedPassword?.id === id ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
-                        
-                      </i>
-                    </td>
-
-                    <td>
-                      <div className="action-button-container">
-                        <DeleteButton onDelete={() => onDelete(id)} />
-                        <EditButton onEdit={() => onEdit(id)} />
-                      </div>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-            <CreateButton onClick={() => setMode("add")}>
-              <AiOutlinePlus />
-            </CreateButton>
-          </>
-        )}
-
-        {mode === "add" && (
-          <AddContactForm
-            formTitle={mode}
-            onCancel={() => setMode("home")}
-            onAdd={onAdd}
-          />
-        )}
-
-        {mode === "edit" && (
-          <AddContactForm
-            formTitle={mode}
-            onCancel={() => setMode("home")}
-            onSave={onSave}
-            contact={editContactData}
-            confirmButton={(contact) => (
-              <>
-                <SaveButton
-                  onSave={() =>
-                    onSave({
-                      ...editContactData,
-                      ...contact,
-                    })
-                  }
-                />
-              </>
-            )}
-          />
-        )}
       </div>
-    </ThemeContexts.Provider>
+
+      {mode === "home" && (
+        <>
+          <div className="contact-container-title">
+            <h1>Contacts</h1>
+            {/* <SearchInput  searchedList={contacts}/> */}
+            <input
+              className="searchInput"
+              placeholder="search..."
+              type="text"
+              onChange={(e) => setSearch(e.target.value)}
+            />
+          </div>
+          <table className={`contact-container-body`}>
+            <thead>
+              <tr>
+                <div className="head-texts">
+                  <th>
+                    Username
+                    <i onClick={() => setSortByNameAZ(!sortByNameAZ)}>
+                      {sortByNameAZ ? (
+                        <AiOutlineSortAscending />
+                      ) : (
+                        <AiOutlineSortDescending />
+                      )}
+                    </i>
+                  </th>
+                  <th>Email<i onClick={() => setSortByEmailAZ(!sortByEmailAZ)}>
+                      {sortByEmailAZ ? (
+                        <AiOutlineSortAscending />
+                      ) : (
+                        <AiOutlineSortDescending />
+                      )}
+                    </i></th>
+                  <th>Password</th>
+                </div>
+                <th>Action</th>
+              </tr>
+            </thead>
+            <tbody>
+              {sortByNameAZ ? sortedList.map(({ id, name, email, password }) => (
+                <tr className="sor" key={id}>
+                  <td>{name}</td>
+                  <td>{email}</td>
+                  <td>
+                    <PasswordInput value={password} />
+                  </td>
+
+                  <td>
+                    <div className="action-button-container">
+                      <DeleteButton onDelete={() => onDelete(id)} />
+                      <EditButton onEdit={() => onEdit(id)} />
+                    </div>
+                  </td>
+                </tr>
+              )) : 
+              sortedListCompare.map(({ id, name, email, password }) => (
+                <tr className="sor" key={id}>
+                  <td>{name}</td>
+                  <td>{email}</td>
+                  <td>
+                    <PasswordInput value={password} />
+                  </td>
+
+                  <td>
+                    <div className="action-button-container">
+                      <DeleteButton onDelete={() => onDelete(id)} />
+                      <EditButton onEdit={() => onEdit(id)} />
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          <CreateButton onClick={() => setMode("add")}>
+            <AiOutlinePlus />
+          </CreateButton>
+        </>
+      )}
+
+      {mode === "add" && (
+        <AddContactForm
+          formTitle={mode}
+          onCancel={() => setMode("home")}
+          onAdd={onAdd}
+        />
+      )}
+
+      {mode === "edit" && (
+        <AddContactForm
+          formTitle={mode}
+          onCancel={() => setMode("home")}
+          onSave={onSave}
+          contact={editContactData}
+          confirmButton={(contact) => (
+            <SaveButton
+              onSave={() =>
+                onSave({
+                  ...editContactData,
+                  ...contact,
+                })
+              }
+            />
+          )}
+        />
+      )}
+    </div>
+    // </ThemeContexts.Provider>
   );
 }
 
